ab1d5e883aae16f08a190a33a6b847569464024c
Anonymous <anonymous@bnp.com>
2024-01-26 19:01:52 +0530

    Refactored toggle button UI

--- app/src/main/java/com/anonymous/berlinclock/featureberlinclock/presentation/berlinclock/ClockScreen.kt
+++ app/src/main/java/com/anonymous/berlinclock/featureberlinclock/presentation/berlinclock/ClockScreen.kt
@@ -2,7 +2,6 @@ package com.anonymous.berlinclock.featureberlinclock.presentation.berlinclock
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,27 +9,19 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.anonymous.berlinclock.R
-import com.anonymous.berlinclock.core.util.TestTags
 import com.anonymous.berlinclock.core.util.TestTags.TOP_BAR
+import com.anonymous.berlinclock.featureberlinclock.presentation.berlinclock.components.ToggleButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -65,29 +56,7 @@ fun ClockScreen() {
                 contentAlignment = Alignment.TopCenter
 
             ) {
-                Row(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .align(Alignment.TopCenter),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = stringResource(R.string.automatic_clock),
-                        style = MaterialTheme.typography.titleLarge,
-                        modifier = Modifier,
-                        textAlign = TextAlign.Center
-                    )
-
-                    var isChecked by remember { mutableStateOf(true) }
-
-                    Switch(
-                        checked = isChecked,
-                        onCheckedChange = { isChecked = it },
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .semantics { contentDescription = TestTags.TOGGLE }
-                    )
-                }
+                ToggleButton()
             }
         }
     }
