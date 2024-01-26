68805e8e9f34fa4bae193b32dff65783b5c7df84
Anonymous <anonymous@bnp.com>
2024-01-26 14:11:58 +0530

    Refactored ClockStateTest class

--- app/src/test/java/com/anonymous/berlinclock/featureberlinclock/presentation/berlinclock/ClockStateTest.kt
+++ app/src/test/java/com/anonymous/berlinclock/featureberlinclock/presentation/berlinclock/ClockStateTest.kt
@@ -3,26 +3,30 @@ package com.anonymous.berlinclock.featureberlinclock.presentation.berlinclock
 import com.anonymous.berlinclock.core.util.HOUR_LAMP_COUNT
 import com.anonymous.berlinclock.core.util.LampColour
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 
 class ClockStateTest {
     private lateinit var clockState: ClockState
 
+    @Before
+    fun setUp() {
+        clockState = ClockState()
+    }
+
+
     @Test
     fun `check secondLamp is initially OFF`() {
-        clockState = ClockState()
         assertThat(clockState.secondLamp == LampColour.OFF).isTrue()
     }
 
     @Test
     fun `check top hour lamps are initially OFF`() {
-        clockState = ClockState()
         assertThat(clockState.topHourLamps == List(HOUR_LAMP_COUNT) { LampColour.OFF }).isTrue()
     }
 
     @Test
     fun `check bottom hour lamps are initially OFF`() {
-        clockState = ClockState()
         assertThat(clockState.bottomHourLamps == List(HOUR_LAMP_COUNT) { LampColour.OFF }).isTrue()
     }
 }
